1b9321d44bf99f1c275c0b274cdcb76b9887bd85
wut <wut@bokecc.com>
2020-04-30 16:33:40 +0800

    first commit

---
+++ src/main/java/com/round/cycle/fun/FunApplication.java
@@ -0,0 +1,13 @@
+package com.round.cycle.fun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunApplication.class, args);
+    }
+
+}
